8eade6b1479931f7bf65b101a183a32f1b458a90
Keitaroh Kobayashi <keita@kbys.me>
2022-05-31 17:08:38 +0900

    add gaiku downloaders

--- README.md
+++ README.md
@@ -27,3 +27,5 @@ pip install -r requirements.txt
 
 * `download_mt_rsdtdsp.py` - 全都道府県、全自治体の「住居表示・住居マスター」データをダウンロードする
 * `download_mt_rsdtdsp_pos.py` - 全都道府県、全自治体の「住居表示－住居マスター位置参照拡張」データをダウンロードする
+* `download_mt_rsdtdsp_blk.py` - 全都道府県、全自治体の「住居表示・街区マスター」データをダウンロードする
+* `download_mt_rsdtdsp_blk_pos.py` - 全都道府県、全自治体の「住居表示－街区マスター位置参照拡張」データをダウンロードする
---
+++ download_mt_rsdtdsp_blk.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+import urllib.request
+from ckanapi import RemoteCKAN
+ua = 'ckanapi/1.0'
+
+digital_cat = RemoteCKAN('https://registry-catalog.registries.digital.go.jp')
+
+# print(f"Search results count: {packages['count']}")
+# print(f"Result array lengeth: {len(packages['results'])}")
+
+packages = []
+
+cursor = 0
+while True:
+  p = digital_cat.action.package_search(fq='+groups:g2-000004', rows=100, start=cursor)
+  if len(p['results']) == 0:
+    break
+
+  packages = packages + p['results']
+
+  cursor += 100
+
+print(f"Loaded metadata for all packages: {len(packages)}")
+
+os.makedirs('./out', exist_ok=True)
+for package in packages:
+  print(f"Downloading resources for: {package['title']}")
+  for resource in package['resources']:
+    print(f"  {resource['name']}")
+    out_dir = os.path.join('./out', os.path.dirname(resource['name']))
+    os.makedirs(out_dir, exist_ok=True)
+    filename = os.path.basename(resource['name'])
+    urllib.request.urlretrieve(
+      resource['url'],
+      os.path.join(out_dir, filename)
+    )
